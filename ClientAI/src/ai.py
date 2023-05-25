e66b26c575f9c0287ca98b52f81ba4b3f44a4b2c
Andrea <andrea.mancion@epitech.eu>
2023-05-25 13:28:21 +0200

    FEAT: add the potentiel connection to the server

--- ClientAI/src/ai.py
+++ ClientAI/src/ai.py
@@ -6,6 +6,8 @@
 ##
 
 import sys
+import socket
+import time
 
 def printHelp():
     print("USAGE: ./zappy_ai -p port -n name -h machine")
@@ -13,9 +15,42 @@ def printHelp():
     print("\tname\tis the name of the team")
     print("\tmachine\tis the name of the machine; localhost by default")
 
+def checkNumber(number):
+    try:
+        int(number)
+        return True
+    except ValueError:
+        return False
+
+def checkString(string):
+    if (string == ""):
+        return False
+    return True
+
+def beginning(port, name, machine):
+    print("Port: " + port)
+    print("Name: " + name)
+    print("Machine: " + machine)
+    ai_socket = socket.socket()
+    ai_socket.connect((machine, int(port)))
+    print("Connected to server")
+    time.sleep(2)
+    ai_socket.close()
+    print("Disconnected from server")
+
 def main(ac, av):
     if (ac == 2 and av[1] == "-help"):
         printHelp()
+    elif(ac == 7):
+        if (av[1] != "-p" or checkNumber(av[2]) == False or av[3] != "-n" or checkString(av[4]) == False or av[5] != "-h"):
+            print("Bad arguments")
+            printHelp()
+            exit(84)
+        beginning(av[2], av[4], av[6])
+    else:
+        print("Bad arguments")
+        printHelp()
+        exit(84)
 
 
 if __name__ == "__main__":
