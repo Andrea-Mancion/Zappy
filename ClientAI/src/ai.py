782bbf2466fe2edbdd3fa896ee8c2e4be2707d06
Andrea <andrea.mancion@epitech.eu>
2023-06-17 17:22:24 +0200

    FIX: fix the command Set object down pass to 'Set object + item' to 'Set + item'

--- ClientAI/src/ai.py
+++ ClientAI/src/ai.py
@@ -28,14 +28,15 @@ def setObjectDown(ai_socket, item):
     # Utiliser la function look pour recuperer les informations des case et split sur les virgules
     # Voici la variable qui va contenir les données du look
     itemsLook = []
+    itemsLook_split = itemsLook[0].split(" ")
     # Voici la variable qui va contenir les données du inventory
     itemsGet = []
     # Check si on est bien seul et sans object sur la case (grace au split)
-    if (len(itemsLook) == 1):
+    if (len(itemsLook_split) == 1):
         for element in itemsGet:
             if (element == item):
                 print("item " + item)
-                ai_socket.send(str.encode("Set object " + item + "\n"))
+                ai_socket.send(str.encode("Set " + item + "\n"))
                 serverString = ai_socket.recv(2046).decode()
                 print("1 " + serverString)
                 if (serverString == "ok"):
@@ -44,57 +45,61 @@ def setObjectDown(ai_socket, item):
                     print("Object not set down")
                 break
 
+# Function to call when we need to Start the elevation
+
+def canSetObject(ai_socket, lvl):
+    if (lvl == 1):
+        setObjectDown(ai_socket, "linemate")
+    elif (lvl == 2):
+        setObjectDown(ai_socket, "linemate")
+        setObjectDown(ai_socket, "deraumere")
+        setObjectDown(ai_socket, "sibur")
+    elif (lvl == 3):
+        setObjectDown(ai_socket, "linemate")
+        setObjectDown(ai_socket, "linemate")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "phiras")
+        setObjectDown(ai_socket, "phiras")
+    elif (lvl == 4):
+        setObjectDown(ai_socket, "linemate")
+        setObjectDown(ai_socket, "deraumere")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "phiras")
+    elif (lvl == 5):
+        setObjectDown(ai_socket, "linemate")
+        setObjectDown(ai_socket, "deraumere")
+        setObjectDown(ai_socket, "deraumere")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "mendiane")
+        setObjectDown(ai_socket, "mendiane")
+        setObjectDown(ai_socket, "menidane")
+    elif (lvl == 6):
+        setObjectDown(ai_socket, "linemate")
+        setObjectDown(ai_socket, "deraumere")
+        setObjectDown(ai_socket, "deraumere")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "phiras")
+    elif (lvl == 7):
+        setObjectDown(ai_socket, "linemate")
+        setObjectDown(ai_socket, "linemate")
+        setObjectDown(ai_socket, "deraumere")
+        setObjectDown(ai_socket, "deraumere")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "sibur")
+        setObjectDown(ai_socket, "mendiane")
+        setObjectDown(ai_socket, "mendiane")
+        setObjectDown(ai_socket, "phiras")
+        setObjectDown(ai_socket, "phiras")
+        setObjectDown(ai_socket, "thystame")
+
 def createClock(ai_socket, name):
     # Avoir le lvl du joueur (recuperer soit au tout debut (base 1) soit a chaque elevation)
     # D'ailleur ne pas oublier de faire ceci que dans le cas ou on veut faire un level up
     lvl = 1
     while not False:
-        if (lvl == 1):
-            setObjectDown(ai_socket, "linemate")
-        elif (lvl == 2):
-            setObjectDown(ai_socket, "linemate")
-            setObjectDown(ai_socket, "deraumere")
-            setObjectDown(ai_socket, "sibur")
-        elif (lvl == 3):
-            setObjectDown(ai_socket, "linemate")
-            setObjectDown(ai_socket, "linemate")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "phiras")
-            setObjectDown(ai_socket, "phiras")
-        elif (lvl == 4):
-            setObjectDown(ai_socket, "linemate")
-            setObjectDown(ai_socket, "deraumere")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "phiras")
-        elif (lvl == 5):
-            setObjectDown(ai_socket, "linemate")
-            setObjectDown(ai_socket, "deraumere")
-            setObjectDown(ai_socket, "deraumere")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "mendiane")
-            setObjectDown(ai_socket, "mendiane")
-            setObjectDown(ai_socket, "menidane")
-        elif (lvl == 6):
-            setObjectDown(ai_socket, "linemate")
-            setObjectDown(ai_socket, "deraumere")
-            setObjectDown(ai_socket, "deraumere")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "phiras")
-        elif (lvl == 7):
-            setObjectDown(ai_socket, "linemate")
-            setObjectDown(ai_socket, "linemate")
-            setObjectDown(ai_socket, "deraumere")
-            setObjectDown(ai_socket, "deraumere")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "sibur")
-            setObjectDown(ai_socket, "mendiane")
-            setObjectDown(ai_socket, "mendiane")
-            setObjectDown(ai_socket, "phiras")
-            setObjectDown(ai_socket, "phiras")
-            setObjectDown(ai_socket, "thystame")
         break
 
 
